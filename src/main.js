41fcea33a63c09112c4c7e75fddf8ea86f0b0e69
Maarlenf <marlenefigueroa.a22@gmail.com>
2023-04-05 19:58:51 -0400

    changes index, main, style

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
 // Este es el punto de entrada de tu aplicacion
+import home from './road/home.js';
+import error from './road/error.js';
 
-import { myFunction } from './lib/index.js';
+const root = document.getElementById('root');
 
-myFunction();
+const routes = [
+  { path: '/', road: home },
+  { path: '/error', road: error },
+];
+
+const defaultRoute = '/';
+
+function navigateTo(hash) {
+  const route = routes.find((routeFind) => routeFind.path === hash);
+
+  if (route && route.road) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+
+    if (route.road() === '') {
+      root.remove(root.innerHTML);
+    }
+    root.innerHTML = route.road(navigateTo);
+  } else {
+    navigateTo('/error');
+  }
+}
+
+window.onpopstate = () => {
+  navigateTo(window.location.pathname);
+};
+
+navigateTo(window.location.pathname || defaultRoute);
---
+++ src/road/error.js
@@ -0,0 +1,5 @@
+function error() {
+  const templateTittle = '<div id="notExist"><h1>Error 404 page not found. Please go home</h1>';
+  return templateTittle;
+}
+export default error;
