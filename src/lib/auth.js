14076da113bb914ee22c852733e1794f8b532aef
Maarlenf <marlenefigueroa.a22@gmail.com>
2023-04-14 12:35:55 -0400

    add chore new components new funtionalites

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,19 +1,29 @@
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+// eslint-disable-next-line no-unused-vars
 import { app } from './firebaseConfig';
 
 export const addAccount = (email, password) => {
+  const change = document.querySelector('.pError');
   const auth = getAuth();
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
     // Signed in
+      // eslint-disable-next-line no-unused-vars
       const user = userCredential.user;
       // console.log('DONE!! ', user);
     // ...
     })
     .catch((error) => {
       const errorCode = error.code;
-      const errorMessage = error.message;
-      // console.log('Mahahahahaha', errorMessage);
-    // ..
+      // const messageError = error.message;
+      if (errorCode === 'auth/weak-password') {
+        change.innerHTML = 'La constraseña ingresada es demasiado débil.';
+      } else if (errorCode === 'auth/email-already-in-use') {
+        change.innerHTML = 'El correo ingresado ya esta en uso.';
+      } else if (errorCode === 'auth/invalid-email') {
+        change.innerHTML = 'El correo ingresado no es válido.';
+      }
+      // console.log(error);
+      // ..
     });
 };
